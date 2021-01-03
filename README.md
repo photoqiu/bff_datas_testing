88292fff08f3af77854c2d26a105bb20f7e36ce7
photoqiu <qiubowinter@vip.sohu.net>
2021-01-03 21:22:16 +0800

    fx

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # bff_datas_testing
 bff order datas use egg.js
+https://github.com/search?p=7&q=topic%3Aegg-plugin&type=Repositories
---
+++ config/plugin.ts
@@ -0,0 +1,26 @@
+exports.static = true;
+
+exports.react = {
+  enable: true,
+  package: 'egg-view-react'
+};
+
+exports.validator = {
+  enable: true,
+  package: 'egg-y-validator'
+};
+
+exports.i18n = {
+  enable: true,
+  package: 'egg-i18n',
+};
+
+exports.cache = {
+  enable: true,
+  package: 'egg-cache'
+};
+
+exports.mongo = {
+  enable: true,
+  package: 'egg-mongo-native'
+};
